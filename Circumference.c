19ae3157052fdeef401a4fb863f431cf1279e4c8
Fernando-Perulles <fernando.perulles@yahoo.com>
2018-10-26 18:03:26 -0500

    Create Circumference.c
    Programa para el cálculo de la circunferencia.

---
+++ Circumference.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define PI 3.14 // Definición del valor que se ocupara para PI.
+
+int main() {
+	
+	float r = 25;
+	float c = PI * (r * r);
+	
+	printf("Radius Value : %f\n", r);
+	printf("Circumference Value : %f\n", c);
+	
+	return 0;
+}
