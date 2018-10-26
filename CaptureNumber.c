ea0babe411d0b5298c29ebcebfb1de574ed73613
Fernando-Perulles <fernando.perulles@yahoo.com>
2018-10-26 17:57:44 -0500

    Create CaptureNumber.c
    En este programa se solicita un número al usuario.

---
+++ CaptureNumber.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+
+int r = 0;
+
+int main ()
+{
+	printf("Ingrese un numero por favor: ");
+	
+	scanf("%d", &r);
+	
+	printf("\n El numero ingresado es %d", r);
+	
+	getch();
+	
+	return 0;
+}
