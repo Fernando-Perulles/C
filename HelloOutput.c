8f9da91f962a4376c50b486c204f98decbda043d
Fernando-Perulles <fernando.perulles@yahoo.com>
2019-01-28 13:55:15 -0600

    Create HelloOutput.c

---
+++ HelloOutput.c
@@ -0,0 +1,21 @@
+# include <stdio.h>
+
+int main() {
+	
+	putchar('H');
+	putchar('e');
+	putchar('l');
+	putchar('l');
+	putchar('o');
+	
+	puts("How are you");
+	
+	printf("today ?\n");
+	
+	printf("%d\n", 4); // Format number as string.
+	
+	printf("%c", 'A');
+	
+	return 0;
+  
+}
